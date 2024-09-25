a72ced6b5f664e44edfab5fa51ca3a352fb3797a
MahmudulHaqueRahat <rahat23fahat@gmail.com>
2024-09-25 15:22:11 +0600

    blog baki'

--- JS/donate.js
+++ JS/donate.js
@@ -6,7 +6,9 @@ tk.addEventListener("click",function(){
     
       if(isNaN(add)||add<=0||add>balance||!Number.isInteger(add)){
         alert('Failed to add money');
+        document.getElementById('amount').value='';
         return;
+         
     }
     else{
              my_modal_5.showModal();
@@ -16,7 +18,15 @@ tk.addEventListener("click",function(){
     const new_donated= donated+add;
     document.getElementById('main-balance').innerText = new_balance;
     document.getElementById('noah').innerText = new_donated;
-     
+
+    const div=document.createElement("div");
+    div.classList.add('bg-white','border-solid','border-2','p-3','card' )
+   div.innerHTML= `
+      <p class="font-bold"> ${add} Taka Donated for Flood at Noakhali, Bangladesh</p> 
+     <p>${new Date().toLocaleString('en-US', { dateStyle: 'full', timeStyle: 'full' })}</p>
+    `;
+    console.log(div);
+    document.getElementById("info").appendChild(div);
 })
 
 const tk1 =document.getElementById("donate1");
@@ -27,16 +37,26 @@ tk1.addEventListener("click",function(){
     
       if(isNaN(add1)||add1<=0||add1>balance){
         alert('Failed to add money');
+        document.getElementById('amount1').value='';
         return;
     }
     else{
                 my_modal_5.showModal();
-                document.getElementById('amount1').value=' ';
+                document.getElementById('amount1').value='';
     } 
     const new_balance= balance-add1;
     const new_donated1= donated1+add1;
     document.getElementById('main-balance').innerText = new_balance;
     document.getElementById('feni').innerText = new_donated1;
+
+    const div=document.createElement("div");
+    div.classList.add('bg-white','border-solid','border-2','p-3','card' )
+   div.innerHTML= `
+      <p class="font-bold"> ${add1} Taka Donated for Flood Relief in Feni,Bangladesh</p> 
+     <p>${new Date().toLocaleString('en-US', { dateStyle: 'full', timeStyle: 'full' })}</p>
+    `;
+    console.log(div);
+    document.getElementById("info").appendChild(div);
 })
 
 const tk2 =document.getElementById("donate2");
@@ -47,15 +67,25 @@ tk2.addEventListener("click",function(event){
     
       if(isNaN(add2)||add2<=0||add2>balance){
         alert('Failed to add money');
+        document.getElementById('amount2').value='';
         return;
     }
     else{
         my_modal_5.showModal();
-        document.getElementById('amount2').value=' ';
+        document.getElementById('amount2').value='';
     } 
     const new_balance= balance-add2;
     const new_donated2= donated2+add2;
     document.getElementById('main-balance').innerText = new_balance;
     document.getElementById('quota').innerText = new_donated2;
+
+    const div=document.createElement("div");
+    div.classList.add('bg-white','border-solid','border-2','p-3','card' )
+   div.innerHTML= `
+      <p class="font-bold"> ${add2} Taka Donated for Injured in the Quota Movement</p> 
+     <p>${new Date().toLocaleString('en-US', { dateStyle: 'full', timeStyle: 'full' })}</p>
+    `;
+    console.log(div);
+    document.getElementById("info").appendChild(div);
 })
 
---
+++ JS/history.js
@@ -0,0 +1,19 @@
+const his= document.getElementById("history");//button
+const ca=document.getElementById("card-1");
+const giv=document.getElementById("give");
+const hlw =document.getElementById("info");
+ his.addEventListener('click',function(){
+    ca.classList.add("hidden"); 
+     
+    his.classList.add("bg-lime-500");
+    giv.classList.remove("bg-lime-500");
+    hlw.classList.remove("hidden");
+
+ })
+ giv.addEventListener('click',function(){
+    ca.classList.remove("hidden"); 
+     his.classList.remove("bg-lime-500");
+     giv.classList.add("bg-lime-500");
+     hlw.classList.add("hidden");
+ })
+// history
